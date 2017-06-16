453d4fcf74da72aa45486098e94203c69129f44d
Rahul De <rahul080327@gmail.com>
2017-06-16 07:06:02 +0530

    Add initial Server

---
+++ README.md
@@ -0,0 +1,23 @@
+[![License: GPL v3](https://img.shields.io/badge/license-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
+[![Build Status](https://travis-ci.org/lispyclouds/bob.svg?branch=master)](https://travis-ci.org/lispyclouds/bob)
+[![codecov](https://codecov.io/gh/lispyclouds/bob/branch/master/graph/badge.svg)](https://codecov.io/gh/lispyclouds/bob)
+
+# Bob the Builder
+
+![](http://vignette2.wikia.nocookie.net/dreamlogos/images/8/8d/Btb1.png/revision/latest?cb=20150801085138)
+
+This is what CI/CD should've been.
+
+## Build requirements
+- Preferably any *nix environment
+- JDK 1.8+
+
+## Running requirements
+- JRE 1.8+
+
+## Testing, building and running
+- Run `./gradlew test` to run tests.
+- Run `./gradlew clean build` to get the standalone JAR.
+- Run `java -jar <JAR_file>` to start the server on port **7777**.
+
+**Bob the Builder image is ©BBC**
---
+++ src/main/kotlin/Server.kt
@@ -0,0 +1,34 @@
+// This file is part of Bob.
+//
+// Bob is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bob is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Bob. If not, see <http://www.gnu.org/licenses/>.
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.ext.web.Router
+
+class Server : AbstractVerticle() {
+    override fun start() {
+        val router = Router.router(vertx)
+
+        router.get("/status/").handler({ request ->
+            request
+                    .response()
+                    .putHeader("content-type", "application/json")
+                    .end("{\"status\": \"Ok\"}")
+        })
+
+        vertx.createHttpServer().requestHandler({
+            router.accept(it)
+        }).listen(8080)
+    }
+}
