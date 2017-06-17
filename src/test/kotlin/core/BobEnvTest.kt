4fee6189007e562dda39a4049d0f9078417b5390
Rahul De <rahul080327@gmail.com>
2017-06-17 14:48:16 +0530

    Add BobEnv

---
+++ src/test/kotlin/core/BobEnvTest.kt
@@ -0,0 +1,42 @@
+// This file is part of Bob.
+//
+// Bob is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bob is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Bob. If not, see <http://www.gnu.org/licenses/>.
+
+package core
+
+import junit.framework.TestCase
+import kotlinx.collections.immutable.immutableMapOf
+import org.junit.Test
+
+
+class BobEnvTest : TestCase() {
+
+    @Test
+    fun testAddEnvVar() {
+        val vars = immutableMapOf(Pair("k1", "v1"))
+        val env = BobEnv(vars)
+        val newEnv = core.addEnvVarIn(env, key="k2", value="v2")
+
+        assertEquals(newEnv.envVars["k2"], "v2")
+    }
+
+    @Test
+    fun testRemoveEnvVar() {
+        val vars = immutableMapOf(Pair("k1", "v1"))
+        val env = BobEnv(vars)
+        val newEnv = core.removeEnvVarFrom(env, "k1")
+
+        assertNull(newEnv.envVars["k1"])
+    }
+}
